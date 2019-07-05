6f86f9f1ab5cc9367f43509b6f957cdecef898ba
Ewelina Gregoraszczuk <gregoraszczuk.e@gmail.com>
2019-07-05 19:11:53 +0200

    added new classes

---
+++ src/com/google/Movie.java
@@ -0,0 +1,12 @@
+package com.google;
+
+public class Movie {
+
+    String title;
+    String description;
+    String director;
+
+    // inicjalizacja zmiennych
+    int rating;
+    boolean oscars;
+}
